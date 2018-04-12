0714db9487d626ac68aa2f8a985fc7cfb83cb989
Tony Le <heytonyle@icloud.com>
2018-04-12 16:42:53 -0700

    Access Lato with Webpack

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import 'typeface-lato';
 import './index.css';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
