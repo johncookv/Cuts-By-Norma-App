773e58627b0e8024148050b35f337f0db3ab9b64
Kevin Fox <creations.foxflare@gmail.com>
2018-05-16 14:54:04 -0700

    bootstrap sticky footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,15 +3,15 @@ import React, {Component} from 'react';
 export default class Footer extends Component {
   render() {
     return (
-      <div className="footer">
+      <footer>
         <div className="container footer-innner">
           <div className="row">
             <div className="col">
-              <div className="py-3">Footer</div>
+              <div>Footer</div>
             </div>
           </div>
         </div>
-      </div>
+      </footer>
     )
   }
 }
